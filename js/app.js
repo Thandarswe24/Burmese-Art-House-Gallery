11c4db44a5767f92485cb33a461e659b60417e71
ThandarSwe24 <thandarswe997@gmail.com>
2023-01-09 21:25:48 +0630

    add code in app.js by tds

--- js/app.js
+++ js/app.js
@@ -1,2 +1,3 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+console.log ("Hello , I'm TDS")
