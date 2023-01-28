df6f403f1791cca85ad9183b55126c9b56a0b5a5
ThandarSwe24 <thandarswe997@gmail.com>
2023-01-28 16:59:22 +0630

    Deploy BAH Page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-console.log ("Hello , I'm TDS")
+/* ======= Header===== */
+
+var elements = document.querySelectorAll(
+  "header > #header_wrapper > nav > ul > li > a"
+);
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
+  /* ======= Header===== */
+
+    /* ======= My Cart===== */
+    function increaseValue() {
+      var value = parseInt(document.getElementById('number').value, 10);
+      value = isNaN(value) ? 0 : value;
+      value++;
+      document.getElementById('number').value = value;
+    }
+    
+    function decreaseValue() {
+      var value = parseInt(document.getElementById('number').value, 10);
+      value = isNaN(value) ? 0 : value;
+      value < 1 ? value = 1 : '';
+      value--;
+      document.getElementById('number').value = value;
+    }
+     /* ======= My Cart===== */
